6bcfb4e842ee32fce6a1e0d7b28b229c09d0d6eb
Ricardo Lopez <ricardolopez93@gmail.com>
2015-05-07 18:31:56 -0400

    -

--- remove-short-seqs.py
+++ remove-short-seqs.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
+from __future__ import print_function
 import screed
 import sys
 
 if len(sys.argv) != 3:
-    print >>sys.stderr, "<py> <fast[aq]> <min_len>"
+    print("<py> <fast[aq]> <min_len>",
+          file=sys.stderr)
     sys.exit(1)
 
 infilename = sys.argv[1]
@@ -12,15 +14,15 @@ min_len = int(sys.argv[2])
 
 def print_seq(seq):
     for i, base in enumerate(seq):
-        sys.stdout.write(base)
+        print(base, end='')
         if (i+1) % 60 == 0:
-            sys.stdout.write("\n")
-    sys.stdout.write("\n")
+            print()
+    print()
 
 for record in screed.open(infilename):
-    name = record['name']
-    sequence = record['sequence']
+    name = record.name
+    sequence = record.sequence
 
     if len(sequence) >= min_len:
-        print ">{}".format(name)
+        print(">", name, sep='')
         print_seq(sequence)
