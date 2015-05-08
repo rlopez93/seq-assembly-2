f1c9995c13cf01428b15d9bbe8c2f702325d99a6
Ricardo Lopez <ricardolopez93@gmail.com>
2015-05-07 22:05:25 -0400

    -

--- coverage.py
+++ coverage.py
@@ -2,45 +2,58 @@
 from __future__ import print_function
 import fileinput
 
-locations = []
+# coverage[i] -> coverage for gene at position i
+coverage = []
 
 for line in fileinput.input():
     fields = line.split()
 
-    # # only error-free reads
-    # if len(fields) > 7:
-    #     continue
+    # the 8th column of fields contains the errors.
+    # if fields has an 8th column, then we ignore it
+    if len(fields) > 7:
+        continue
 
+    # get location of current read from map file
     location = int(fields[3])
-    locations.append(location)
+    length = 36
 
-loc_max = max(locations)
-loc_len = loc_max + 1 + 36
+    # calculate difference between end
+    # of current read and size of coverage
+    diff = location + length - len(coverage)
 
-coverage = [0] * loc_len
+    # if current read extends past current coverage
+    if diff > 0:
+        # extend coverage to make up the difference
+        coverage.extend([0] * diff)
 
-for location in locations:
-    for i in range(location, location+36):
+    # update coverage
+    for i in xrange(location, location+length):
         coverage[i] += 1
+
+# now we find what intervals of the genome
+# have non-zero coverage
 
 i = 0
 while i < len(coverage):
+    # ignore positions with 0 coverage
     while i < len(coverage) and coverage[i] == 0:
         i += 1
 
     if i >= len(coverage):
         break
 
+    # set start point for current coverage interval
     start = i
     sub_coverage = [coverage[i]]
 
+    # find size of current coverage interval
     i += 1
     while i < len(coverage) and coverage[i] != 0:
         sub_coverage.append(coverage[i])
         i += 1
 
     total = len(sub_coverage)
-    avg_cov = sum(sub_coverage) / total
+    avg_cov = sum(sub_coverage) / float(total)
 
     print("{0:4d} {1:10d} {2:6.1f}"
           .format(total, start, avg_cov))
